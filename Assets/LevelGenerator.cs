e700dbab19b9b50879a8b005c93504498af0bdf9
Eric Marty <contact@ericmarty.co>
2017-11-25 14:52:26 -0800

    Reading pixel data from png file

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	// This will allow us to drag an image into this slot - Brackeys
+	public Texture2D map;
+
+	// Use this for initialization
+	void Start () {
+		GenerateLevel();
+	}
+
+	void GenerateLevel()
+	{
+		for (int x = 0; x < map.width; x++)
+		{
+			for (int y = 0; y < map.height; y++)
+			{
+				GenerateTile(x, y);
+			}
+		}
+	}
+
+	void GenerateTile(int x, int y)
+	{
+		// Get color at pixel on image
+		Color pixelColor = map.GetPixel(x, y);
+
+		if (pixelColor.a == 0)
+		{
+			// alpha is 0, pixel is transparent so ignore it
+			return;
+		}
+
+		Debug.Log(pixelColor);
+	}
+	
+}
